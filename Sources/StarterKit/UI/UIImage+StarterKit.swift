187ec014323f832a054e33d93851832187b46d32
Emilio Pavia <emilio.pavia@gmail.com>
2021-01-14 17:20:07 +0100

    Add border generation to UIImage extension

--- Sources/StarterKit/UI/UIImage+StarterKit.swift
+++ Sources/StarterKit/UI/UIImage+StarterKit.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 public extension UIImage {
-    class func image(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage? {
+    class func image(color: UIColor,
+                     size: CGSize = CGSize(width: 1, height: 1),
+                     cornerRadius: CGFloat = 0,
+                     borderColor: UIColor? = nil,
+                     borderWidth: CGFloat = 0) -> UIImage? {
+        
         UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        
         color.setFill()
-        UIRectFill(CGRect(origin: CGPoint.zero, size: size))
+        let rect = CGRect(origin: CGPoint.zero, size: size)
+        let path = UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius)
+        path.fill()
+        
+        if let borderColor = borderColor, borderWidth > 0 {
+            borderColor.setStroke()
+            let borderRect = rect.insetBy(dx: borderWidth / 2.0, dy: borderWidth / 2.0)
+            let borderPath = UIBezierPath(roundedRect: borderRect, cornerRadius: cornerRadius)
+            borderPath.stroke()
+        }
+        
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return image
