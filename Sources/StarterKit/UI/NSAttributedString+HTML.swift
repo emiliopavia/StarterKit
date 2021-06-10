0ea1315d3ede4dd2cabfe5e3bf1b7b9acea51f4f
Emilio Pavia <emilio.pavia@gmail.com>
2021-06-10 15:55:06 +0200

    Update NSAttributedString+HTML

--- Sources/StarterKit/UI/NSAttributedString+HTML.swift
+++ Sources/StarterKit/UI/NSAttributedString+HTML.swift
@@ -12,11 +12,17 @@ extension NSAttributedString {
     public convenience init?(htmlBody: String?,
                              font: UIFont,
                              color: UIColor,
-                             alignment: NSTextAlignment = .left) {
+                             tintColor: UIColor? = nil,
+                             lineHeight: Float? = nil,
+                             alignment: NSTextAlignment = .left,
+                             layoutDirection: UITraitEnvironmentLayoutDirection = .leftToRight) {
         guard let body = htmlBody else { return nil }
         
         var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
         color.getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        var tr: CGFloat = 0, tg: CGFloat = 0, tb: CGFloat = 0, ta: CGFloat = 0
+        (tintColor ?? color).getRed(&tr, green: &tg, blue: &tb, alpha: &ta)
         
         // FIXME: https://openradar.appspot.com/6153065
         let fontFamily: String
@@ -26,26 +32,31 @@ extension NSAttributedString {
             fontFamily = font.familyName
         }
         
+        let lh = lineHeight != nil ? "\(lineHeight!)px" : "normal"
+        
         let document = """
-        <html>
-        <head>
-        <style>
-            a {
-                text-decoration: inherit;
-            }
-            html * {
-                color: rgba(\(r * 255.0), \(g * 255.0), \(b * 255.0), \(a)) !important;
-                font-family: \(fontFamily) !important;
-                font-size: \(font.pointSize) !important;
-                text-align: \(alignment.htmlTextAlignment);
-            }
-        </style>
-        </head>
-        <body>
-        \(body)
-        </body>
-        </html>
-        """
+          <html>
+          <head>
+          <style>
+              html * {
+                  color: rgba(\(r * 255.0), \(g * 255.0), \(b * 255.0), \(a));
+                  font-family: \(fontFamily) !important;
+                  font-size: \(font.pointSize) !important;
+                  line-height: \(lh);
+                  text-align: \(alignment.htmlTextAlignment);
+                  direction: \(layoutDirection.htmlDirection);
+              }
+              a {
+                  color: rgba(\(tr * 255.0), \(tg * 255.0), \(tb * 255.0), \(ta));
+                  font-weight: bold;
+              }
+          </style>
+          </head>
+          <body>
+          \(body)
+          </body>
+          </html>
+          """
         
         try? self.init(data: document.data(using: String.Encoding.utf8)!,
                        options: [.documentType: NSAttributedString.DocumentType.html,
@@ -65,6 +76,17 @@ extension NSTextAlignment {
             return "justify"
         default:
             return "left"
+        }
+    }
+}
+
+extension UITraitEnvironmentLayoutDirection {
+    var htmlDirection: String {
+        switch self {
+        case .rightToLeft:
+            return "rtl"
+        default:
+            return "ltr"
         }
     }
 }
